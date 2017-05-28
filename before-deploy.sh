33996baf98356aa07ace94fd1ae26f435096be6c
gidis <gidis@liveperson.com>
2017-05-28 22:59:41 +0300

    run build java

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in codesigning.asc -out signingkey.asc -d
+    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in codesigning.asc.enc -out signingkey.asc -d
     gpg --fast-import signingkey.asc
 fi
